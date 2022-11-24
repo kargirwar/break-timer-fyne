3641434e7d2caa009199781ba2926b5a58ec4534
Pankaj Kargirwar <kargirwar@protonmail.com>
2022-11-24 11:34:20 +0530

    Using oto

--- go.mod
+++ go.mod
@@ -13,10 +13,11 @@ require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
 	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/hajimehoshi/oto/v2 v2.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,88 +1,73 @@
 package main
 
 import (
-	_ "fmt"
-	"net/http"
-	"os"
-	"runtime"
-	"strconv"
-	"time"
+    "time"
+    "os"
+    "bytes"
 
-	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
-	"gopkg.in/natefinch/lumberjack.v2"
+    "github.com/hajimehoshi/go-mp3"
+    "github.com/hajimehoshi/oto/v2"
 )
 
-var VERSION = "VERSION"
-
-const PORT = 22890
-const LOG_FILE = "break-timer.log"
-const SETTINGS_FILE = "settings.json"
-const PLAY = "play"
-const STOP = "stop"
-
-var logger *lumberjack.Logger
-var timerCh chan string
-var playerCh chan string
-
-func init() {
-	timerCh = make(chan string)
-	playerCh = make(chan string)
-
-	log.SetFormatter(&log.JSONFormatter{
-		TimestampFormat: time.StampMilli,
-	})
-
-	logger = &lumberjack.Logger{
-		Filename:   getOsFilePath(LOG_FILE),
-		MaxSize:    1, //megabytes
-		MaxBackups: 1,
-		MaxAge:     1, //days
-		Compress:   true,
-	}
-	log.SetOutput(logger)
-
-	log.SetLevel(log.DebugLevel)
-}
-
-func getOsFilePath(f string) string {
-	//Linux
-	if runtime.GOOS == "linux" {
-		return "/var/log/break-timer/" + f
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return f
-	}
-
-	//OSX
-	if runtime.GOOS == "darwin" {
-		return home + "/Library/BreakTimer/" + f
-	}
-
-	return f
-}
-
 func main() {
+    // Read the mp3 file into memory
+    fileBytes, err := os.ReadFile("./beep.mp3")
+    if err != nil {
+        panic("reading my-file.mp3 failed: " + err.Error())
+    }
 
-	go start()
-	go play()
+    // Convert the pure bytes into a reader object that can be used with the mp3 decoder
+    fileBytesReader := bytes.NewReader(fileBytes)
 
-	r := mux.NewRouter()
-	r.Use(mw)
+    // Decode file
+    decodedMp3, err := mp3.NewDecoder(fileBytesReader)
+    if err != nil {
+        panic("mp3.NewDecoder failed: " + err.Error())
+    }
 
-	//routes
-	//r.HandleFunc("/about", about).Methods(http.MethodGet, http.MethodOptions)
-	r.HandleFunc("/set-timers", setTimers).Methods(http.MethodPost, http.MethodOptions)
-	r.HandleFunc("/stop", stop).Methods(http.MethodGet, http.MethodOptions)
+    // Prepare an Oto context (this will use your default audio device) that will
+    // play all our sounds. Its configuration can't be changed later.
 
-	http.Handle("/", r)
+    // Usually 44100 or 48000. Other values might cause distortions in Oto
+    samplingRate := 44100
 
-	log.Info("bt-agent Listening at:" + strconv.Itoa(PORT))
+    // Number of channels (aka locations) to play sounds from. Either 1 or 2.
+    // 1 is mono sound, and 2 is stereo (most speakers are stereo). 
+    numOfChannels := 2
 
-	if err := http.ListenAndServe(":"+strconv.Itoa(PORT), nil); err != nil {
-		log.Fatal(err.Error())
-		os.Exit(-1)
-	}
+    // Bytes used by a channel to represent one sample. Either 1 or 2 (usually 2).
+    audioBitDepth := 2
+
+    // Remember that you should **not** create more than one context
+    otoCtx, readyChan, err := oto.NewContext(samplingRate, numOfChannels, audioBitDepth)
+    if err != nil {
+        panic("oto.NewContext failed: " + err.Error())
+    }
+    // It might take a bit for the hardware audio devices to be ready, so we wait on the channel.
+    <-readyChan
+
+    // Create a new 'player' that will handle our sound. Paused by default.
+    player := otoCtx.NewPlayer(decodedMp3)
+    
+    // Play starts playing the sound and returns without waiting for it (Play() is async).
+    player.Play()
+
+    // We can wait for the sound to finish playing using something like this
+    for player.IsPlaying() {
+        time.Sleep(time.Millisecond)
+    }
+
+    // Now that the sound finished playing, we can restart from the beginning (or go to any location in the sound) using seek
+    // newPos, err := player.(io.Seeker).Seek(0, io.SeekStart)
+    // if err != nil{
+    //     panic("player.Seek failed: " + err.Error())
+    // }
+    // println("Player is now at position:", newPos)
+    // player.Play()
+
+    // If you don't want the player/sound anymore simply close
+    err = player.Close()
+    if err != nil {
+        panic("player.Close failed: " + err.Error())
+    }
 }
