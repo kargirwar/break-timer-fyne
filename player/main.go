3d44b7e4a9becc1b54655dc61338fb49df46a324
Pankaj Kargirwar <kargirwar@protonmail.com>
2022-11-26 09:56:34 +0530

    Added count and interval to player

--- player/main.go
+++ player/main.go
@@ -2,25 +2,26 @@ package player
 
 import (
 	"time"
-	"log"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"bytes"
 	_ "embed"
 
 	"github.com/hajimehoshi/go-mp3"
 	"github.com/hajimehoshi/oto/v2"
-	"github.com/tevino/abool/v2"
 )
 
 //go:embed beep.mp3
 var fileBytes []byte
-var isPlaying *abool.AtomicBool
 var player oto.Player
+var decodedMp3 *mp3.Decoder
+var otoCtx *oto.Context
 
 func init() {
-	isPlaying = abool.New()
 	fileBytesReader := bytes.NewReader(fileBytes)
 
-	decodedMp3, err := mp3.NewDecoder(fileBytesReader)
+	err := *new(error)
+	decodedMp3, err = mp3.NewDecoder(fileBytesReader)
 	if err != nil {
 		panic("mp3.NewDecoder failed: " + err.Error())
 	}
@@ -35,48 +36,50 @@ func init() {
 	audioBitDepth := 2
 
 	// Remember that you should **not** create more than one context
-	otoCtx, readyChan, err := oto.NewContext(samplingRate, numOfChannels, audioBitDepth)
+	readyChan := make(chan struct{})
+	otoCtx, readyChan, err = oto.NewContext(samplingRate, numOfChannels, audioBitDepth)
 	if err != nil {
 		panic("oto.NewContext failed: " + err.Error())
 	}
 	// It might take a bit for the hardware audio devices to be ready, so we wait on the channel.
 	<-readyChan
 
+}
+
+//play times with interval milliseconds in between
+func Play(times, interval int) {
+	log.Info("Playing")
 	player = otoCtx.NewPlayer(decodedMp3)
+	ticker := time.NewTicker(time.Duration(interval) * time.Millisecond)
+
+	count := 0;
+	loop:
+	for {
+		select {
+		case <-ticker.C:
+			play(player)
+
+			count++
+			if count == times {
+				ticker.Stop()
+				break loop
+			}
+		}
+	}
 }
 
-func Play() {
-	if isPlaying.IsSet() {
-		log.Println("Already playing")
-		return
+func play(player oto.Player) {
+	_, err := player.(io.Seeker).Seek(0, io.SeekStart)
+	if err != nil{
+		panic("player.Seek failed: " + err.Error())
 	}
-
-	go play()
-
-	for isPlaying.IsNotSet() {
-		time.Sleep(time.Millisecond)
-	}
-}
-
-func play() {
-	log.Println("Playing");
-	isPlaying.Set()
 	player.Play()
-
 	for player.IsPlaying() {
 		time.Sleep(time.Millisecond)
 	}
-
-	isPlaying.UnSet();
-	log.Println("Done");
 }
 
 func Close() {
-	for isPlaying.IsSet() {
-		time.Sleep(time.Millisecond)
-	}
-
-	log.Println("Closing");
 	err := player.Close()
 	if err != nil {
 		panic("player.Close failed: " + err.Error())
